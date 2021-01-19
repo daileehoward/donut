9fe871f812b693fd490f2d32f6933e72a0e2a169
ttran112 <ttran112@mail.greenriver.edu>
2021-01-19 14:33:21 -0500

    hopefully it was a success

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Thanh and Dailee</h1>
+    <p>Hello Dailee this is just a test </p>
+</body>
+</html>
